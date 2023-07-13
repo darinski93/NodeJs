5984fc0ca5503fbd23a8c9393bb7b90d025d8137
Darios Petrov <darios.petrov93@gmail.com>
2023-07-13 23:58:05 +0300

    adding home page, creating server

---
+++ IntroToNodeJs/catShelter/index.js
@@ -0,0 +1,40 @@
+const http = require('http')
+
+const homePage = require('./views/home')
+const siteCss = require('./css/site.css')
+
+
+const server = http.createServer((req, res) => {
+
+
+    res.writeHead(200, {
+        'content-type': 'text/html',
+    })
+
+    switch (req.url) {
+        case '/':
+            res.write(homePage)
+            break;
+
+        case './css/site.css':
+
+            res.writeHead(200, {
+                'content-type': 'text/css',
+            })
+            res.write(siteCss)
+
+          
+
+        default:
+            `<h1>404<h1>`
+            break;
+    }
+
+
+    res.end()
+
+})
+
+server.listen(5000)
+
+console.log('Server is running on port 5000...');
