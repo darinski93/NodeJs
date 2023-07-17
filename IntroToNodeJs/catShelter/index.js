2900e836eea9dde7af0fc0b3150dc8c64b2c4113
Darios Petrov <darios.petrov93@gmail.com>
2023-07-18 02:36:22 +0300

    adding json

--- IntroToNodeJs/catShelter/index.js
+++ IntroToNodeJs/catShelter/index.js
@@ -1,39 +1,46 @@
 const http = require('http')
 
-const homePage = require('./views/home')
-const siteCss = require('./css/site.css')
+
+const cats = require('./cats.json')
+const homePage = require('./views/home.js')
+const siteCss = require('./css/site.css.js')
 const addBreed = require('./views/addBread')
+const editCat = require('./views/editCat')
+
 
 
 const server = http.createServer((req, res) => {
-
 
     res.writeHead(200, {
         'content-type': 'text/html',
     })
 
+    if (req.url == '/') {
+        res.write(homePage)
+
+    } else if(/cats\/d+\/add-bread/.test(req.url)){
+
+        let catId = req.url.split('/')[2]
+        let cat = cats.find(x => x.id === catId)
+        res.write(editCat(cat))
+
+    }
+    
+    
+    else if(req.url == '/css/site.css') {
+
+        res.writeHead(200, {
+            'content-type': 'text/css',
+        })
+        res.write(siteCss)
 
 
-    switch (req.url) {
-        case '/':
-            res.write(homePage)
-            break;
+    } else if (req.url == '/cats/add-breed') {
 
-        case '/css/site.css':
+        res.write(addBreed)
 
-            res.writeHead(200, {
-                'content-type': 'text/css',
-            })
-            res.write(siteCss)
-
-        case '/cats/add-breed':
-            res.write(addBreed)
-            break;
-
-
-        default:
-            `<h1>404<h1>`
-            break;
+    } else {
+        `<h1>404<h1>`
     }
 
 
