e36a7104d904afecef58767ffe7146f3d9a6bdf0
Darios Petrov <darios.petrov93@gmail.com>
2023-07-18 13:20:50 +0300

    adding views using fs

--- IntroToNodeJs/catShelter/index.js
+++ IntroToNodeJs/catShelter/index.js
@@ -1,52 +1,62 @@
 const http = require('http')
+const fs = require('fs/promises')
+const path = require('path')
 
 
 const cats = require('./cats.json')
-const homePage = require('./views/home.js')
-const siteCss = require('./css/site.css.js')
-const addBreed = require('./views/addBread')
-const editCat = require('./views/editCat')
+// const siteCss = require('./css/site.css.js')
 
 
 
-const server = http.createServer((req, res) => {
+
+const server = http.createServer(async (req, res) => {
 
     res.writeHead(200, {
         'content-type': 'text/html',
     })
 
     if (req.url == '/') {
+        const homePage = await readFile('./views/home.html')
+
         res.write(homePage)
 
-    } else if(/cats\/d+\/add-bread/.test(req.url)){
-
-        let catId = req.url.split('/')[2]
-        let cat = cats.find(x => x.id === catId)
-        res.write(editCat(cat))
+    } else if (/cats\/\d+\/edit/.test(req.url)) {
+        let catId = req.url.split('/')[2];
+        let cat = cats.find(x => x.id == catId);
+        // res.write(catShelter(cat))
 
     }
-    
-    
-    else if(req.url == '/css/site.css') {
+
+
+    else if (req.url == '/css/site.css') {
 
         res.writeHead(200, {
             'content-type': 'text/css',
         })
+
+        const siteCss = await readFile('./content/styles/site.css')
         res.write(siteCss)
 
 
     } else if (req.url == '/cats/add-breed') {
 
-        res.write(addBreed)
+        // res.write(addBreed)
 
     } else {
-        `<h1>404<h1>`
+        // `<h1>404<h1>`
     }
 
 
     res.end()
 
 })
+
+
+function readFile(path) {
+
+    return fs.readFile(path, { encoding: 'utf-8' })
+
+}
 
 server.listen(5000)
 
