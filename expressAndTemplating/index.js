baa7c509f4b3bff7b81fc7174bfe1dd577aa7953
Darios Petrov <darios.petrov93@gmail.com>
2023-07-20 13:55:28 +0300

    adding basic endpoints

--- expressAndTemplating/index.js
+++ expressAndTemplating/index.js
@@ -3,9 +3,40 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
+    res.send(`<h1>Hello from Express!</h1>
+    <a href="/cats">Cats</a>
+    <a href="/dogs">Dogs</a>
 
-    res.send('Hello from Express!')
     
+    `)
+})
+
+app.get('/cats', (req, res) => {
+    res.send(`<h1>Cats Page</h1>
+    <a href="/">Home</a>
+    <a href="/dogs">Dogs</a>
+
+    `)
+})
+
+app.get('/dogs', (req, res) => {
+    res.send(`<h1>Dogs Page </h1>
+    <a href="/">Home</a>
+    <a href="/cats">Cats</a>
+
+    `)
+})
+
+app.post('/cats', (req,res) =>{
+    res.send('cat received')
+})
+
+app.put('/cats', (req,res) =>{
+    res.send('cat is updated')
+})
+
+app.delete('/cats', (req, res) =>{
+    res.send('cat is deleted')
 })
 
 app.listen(5000, () => console.log('Server is running on port 5000...'));
